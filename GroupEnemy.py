b7e8d06fe8a0727897d5a9e2c5e24287b435cb74
여현오 <hyunohyeo@wifi-10-40-173-68.wifi.berkeley.edu>
2025-08-26 14:28:40 -0700

    Group enemy 구현

--- GroupEnemy.py
+++ GroupEnemy.py
@@ -1,30 +1,50 @@
 import pygame
-import Enemy
+from Enemy import *
+import math
+import random
 
 class GroupEnemy:
-    image = "Image/Enemy.png"
     
-    def __init__(self, n):
+    def __init__(self, n, image="Image/Enemy.png"):
         self.group = []
 
-        for _ in range(n):
-            self.group.append(Enemy(self.image))
+        self.x = random.randint(0, 1216 - (64 * n))
+        self.y = 0
 
-        self.count = n
-
+        self.image = image
         
-    def move(self):
-        return
+        for i in range(n):
+            self.group.append(Enemy(self.image, (self.x + (i * 64)), self.y))
+        
+    #move enemy until their terminate position
+    def move(self, dx, dy):
+        for enemy in self.group:
+            enemy.move(dx, dy)
     
+    #display undestroyed enemies
     def display(self, screen):
         for enemy in self.group:
             screen.blit(enemy.image, (enemy.x, enemy. y))
 
-    def checkCollision(self):
-        def isCollision():
-            return
+    #Check for collision and return score to be added
+    def checkCollision(self, bullet):
+        count = 0
+
+        def isCollision(target, object):
+            distance = math.sqrt(math.pow((target.x + 24) - object.x, 2) + math.pow(target.y - object.y, 2))
+
+            if distance < 27:
+                return True
+            else:
+                return False
+            
+        for enemy in self.group:
+            if isCollision(enemy, bullet):
+                self.group.remove(enemy)
+
+                count += 1
         
-        return
+        return count
 
     def isDestroyed(self):
         return len(self.group) == 0
