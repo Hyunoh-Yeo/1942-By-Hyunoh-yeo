a7af23a5d43dfc42c145897ad64dcd7be3e17da6
Hyunoh-Yeo <hyunoh.yeo@gmail.com>
2026-01-15 16:08:04 -0800

    Bullet

--- Bullet.py
+++ Bullet.py
@@ -1,12 +1,21 @@
 import pygame
 
 class Bullet:
-    def __init__(self, image, playerX, playerY):
+    def __init__(self, image, x, y):
         self.image = pygame.image.load(image)
 
-        self.x = playerX + 32
-        self.y = playerY
+        self.x = x
+        self.y = y
+        self.y_change = 0.5
 
         self.bullet_state = "ready"
 
+    def fire(self, screen, x, y):
+        self.bullet_state = "fire"
 
+        self.x = x
+        self.y = y
+
+        screen.blit(self.image, (x, y))
+
+ 
